73d74e5eb79dbe50770cb7e66839199a6d3c357a
Abdullah <abdullah.iqqbal487@gmail.com>
2024-08-12 14:33:20 +0500

    section-1

---
+++ src/pages/section1/index.jsx
@@ -0,0 +1,16 @@
+import "./style.css";
+
+const Index = () => {
+  return (
+    <section>
+      <div className="row">
+        <div className="bg-img">
+          <h1>Work With Us</h1>
+        </div>
+      </div>
+    </section>
+    
+  );
+};
+
+export default Index;
