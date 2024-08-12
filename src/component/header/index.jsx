ca9e40b20141ff5ae9f0dc03f055cd530bd8430f
Abdullah <abdullah.iqqbal487@gmail.com>
2024-08-12 14:38:45 +0500

    header components

--- src/component/header/index.jsx
+++ src/component/header/index.jsx
@@ -1,13 +1,16 @@
 import "./style.css";
-import IconCalls from "../../assets/images/Group (2).png"
-import IconCall from "../../assets/images/fi_542689.png"
-import LinkCall from "../../assets/images/fi_6964169.png"
-import LinkCalls from "../../assets/images/Group (3).png"
-import HeadLink from "../../assets/images/Vector (5).png"
-import HeadLinks from "../../assets/images//fi_20837 (1).png"
-import MenuImg from "../../assets/images/Layer_1.png"
+import IconCalls from "../../assets/images/Group (2).png";
+import IconCall from "../../assets/images/fi_542689.png";
+import LinkCall from "../../assets/images/fi_6964169.png";
+import LinkCalls from "../../assets/images/Group (3).png";
+import HeadLink from "../../assets/images/Vector (5).png";
+import HeadLinks from "../../assets/images//fi_20837 (1).png";
+import MenuImg from "../../assets/images/Layer_1.png";
+import Button from "../button";
+
 const Header = () => {
   return (
+    
     <header>
       <div className="top-bar">
         <div className="container">
@@ -33,7 +36,7 @@ const Header = () => {
               <ul className="icon-list">
                 <li>
                   <a href="#">
-                    <img src={LinkCall}alt="" />
+                    <img src={LinkCall} alt="" />
                     Carrier Portal
                   </a>
                 </li>
@@ -91,7 +94,7 @@ const Header = () => {
               </ul>
             </div>
             <div className="quote">
-              <button>Get a Quote</button>
+           <Button text={'get a quote'} />
             </div>
           </div>
         </div>
