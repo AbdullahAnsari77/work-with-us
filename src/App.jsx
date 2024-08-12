f09f14f636979ad1b3ddc282213cf3bed89917e3
Abdullah <abdullah.iqqbal487@gmail.com>
2024-08-12 14:34:34 +0500

    App jsx and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import './App.css'
+// import Button from './component/button'
 import Header from './component/header'
-
+import Index from './pages/section1'
+import { Section2 } from './pages/section2'
+import Section3 from './pages/section3'
+import Section4 from './pages/section4'
+import Section5 from './pages/section5'
+import Section6 from './pages/section6'
 function App() {
 
   return (
-    <div className='App'>
+    // <div className='App'>
+    <>
       <Header />
-    </div>
+      <Index/>
+      <Section2/>
+      <Section3/>
+      <Section4/>
+      <Section5/>
+      <Section6/>
+      </>
+      
+      
+    // </div>
   )
 }
 
