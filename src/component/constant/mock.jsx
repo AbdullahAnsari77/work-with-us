408682847a389636ffd80d6ef5e7b68a6436e5f9
Abdullah <abdullah.iqqbal487@gmail.com>
2024-08-15 14:22:09 +0500

    created map data

---
+++ src/component/constant/mock.jsx
@@ -0,0 +1,89 @@
+import VectorImg from '../../assets/images/Vector (9).png'
+import fi_4671092 from '../../assets/images/fi_4671092 (1).png'
+import basket from '../../assets/images/basket.png'
+import truckter from '../../assets/images/truckter.png'
+export const box=[
+    {
+    image:VectorImg,
+    title:"Construction",
+    discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
+    },
+    {
+        image:fi_4671092,
+        title:"Automotive",
+        discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
+        },
+        {
+            image:basket,
+            title:"Retail",
+            discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
+            },
+            {
+                image:"truckter",
+                title:"Agriculture",
+                discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
+                },
+                {
+                    image:"Vector (9)",
+                    title:"Manufacturing",
+                    discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
+                    },
+                    {
+                        image:"Vector (9)",
+                        title:"Food & Beverage",
+                        discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
+                        }, {
+                            image:"Vector (9)",
+                            title:"Medical Equipment - Healthcare Products",
+                            discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
+                            },
+
+                            {
+                                image:"Vector (9)",
+                                title:"Aerospace",
+                                discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
+                                },
+
+                                {
+                                    image:"Vector (9)",
+                                    title:"Perishables",
+                                    discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
+                                    },
+
+                                    {
+                                        image:"Vector (9)",
+                                        title:"Chemicals & Plastics",
+                                        discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
+                                        },
+
+                                        {
+                                            image:"Vector (9)",
+                                            title:"Gaming Equipment",
+                                            discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
+                                            },
+
+                                            {
+                                                image:"Vector (9)",
+                                                title:"Trade Shows",
+                                                discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
+                                                },
+
+                                                {
+                                                    image:"Vector (9)",
+                                                    title:"Entertainment",
+                                                    discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
+                                                    },
+
+                                                    {
+                                                        image:"Vector (9)",
+                                                        title:"Technology & Electronics",
+                                                        discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
+                                                        },
+
+                                                        {
+                                                            image:"Vector (9)",
+                                                            title:"Sports Equipment",
+                                                            discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
+                                                            },
+
+];
