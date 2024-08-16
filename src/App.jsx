577cc5985c32fd71692833c3e5f62a91ab96583a
Abdullah <abdullah.iqqbal487@gmail.com>
2024-08-16 18:58:53 +0500

    commit code remove

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,23 @@
-import './App.css'
-// import Button from './component/button'
-import Header from './component/header'
-import Index from './pages/section1'
-import { Section2 } from './pages/section2'
-import Section3 from './pages/section3'
-import Section4 from './pages/section4'
-import Section5 from './pages/section5'
-import Section6 from './pages/section6'
+import "./App.css";
+import Header from "./component/header";
+import Section1 from "./pages/section1";
+import Section2 from "./pages/section2";
+import Section3 from "./pages/section3";
+import Section4 from "./pages/section4";
+import Section5 from "./pages/section5";
+import Section6 from "./pages/section6";
 function App() {
-
   return (
-    // <div className='App'>
     <>
       <Header />
-      <Index/>
-      <Section2/>
-      <Section3/>
-      <Section4/>
-      <Section5/>
-      <Section6/>
-      </>
-      
-      
-    // </div>
-  )
+      <Section1 />
+      <Section2 />
+      <Section3 />
+      <Section4 />
+      <Section5 />
+      <Section6 />
+    </>
+  );
 }
 
-export default App
+export default App;
