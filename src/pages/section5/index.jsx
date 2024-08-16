b5d0031b7209fee1a6a0beb9a26fae63202067a1
Abdullah <abdullah.iqqbal487@gmail.com>
2024-08-16 19:43:45 +0500

    formating section-5

--- src/pages/section5/index.jsx
+++ src/pages/section5/index.jsx
@@ -6,31 +6,38 @@ import "./style.css";
 
 function Section5() {
   return (
-  <section className="solution">
-    <div className="container">
-    <h2>Solutions</h2>
-    <div className="row">
-            <div className="column-5">
-             <a href="#"><img src={LinkImg} alt="" className="link-img-a" /></a>
-             <p>Full Truckload</p>
-            </div>
-            <div className="column-5">
-             <a href="#"><img src={LinkImgA} alt=""  className="link-img-b" /></a>
-             <p>Refrigerated (Reefer)</p>
-            </div>
-            <div className="column-5">
-             <a href="#"><img src={LinkImgB} alt=""  className="link-img-c" /></a>
-             <p>Flatbed</p>
-            </div>
-            <div className="column-5">
-             <a href="#"><img src={LinkImgC} alt=""  className="link-img-d" /></a>
-             <p>Expedited</p>
-            </div>
-         
+    <section className="solution">
+      <div className="container">
+        <h2>Solutions</h2>
+        <div className="row">
+          <div className="column-5">
+            <a href="#">
+              <img src={LinkImg} alt="" className="link-img-a" />
+            </a>
+            <p>Full Truckload</p>
+          </div>
+          <div className="column-5">
+            <a href="#">
+              <img src={LinkImgA} alt="" className="link-img-b" />
+            </a>
+            <p>Refrigerated (Reefer)</p>
+          </div>
+          <div className="column-5">
+            <a href="#">
+              <img src={LinkImgB} alt="" className="link-img-c" />
+            </a>
+            <p>Flatbed</p>
+          </div>
+          <div className="column-5">
+            <a href="#">
+              <img src={LinkImgC} alt="" className="link-img-d" />
+            </a>
+            <p>Expedited</p>
+          </div>
         </div>
-    </div>
-  </section>
-  )
+      </div>
+    </section>
+  );
 }
 
-export default Section5
+export default Section5;
