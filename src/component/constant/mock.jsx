ddc187fa3299aa348f6f41e47955aa489bfe35e1
Abdullah <abdullah.iqqbal487@gmail.com>
2024-08-16 19:46:00 +0500

    formating and create component of link

--- src/component/constant/mock.jsx
+++ src/component/constant/mock.jsx
@@ -1,89 +1,159 @@
-import VectorImg from '../../assets/images/Vector (9).png'
-import fi_4671092 from '../../assets/images/fi_4671092 (1).png'
-import basket from '../../assets/images/basket.png'
-import truckter from '../../assets/images/truckter.png'
-export const box=[
-    {
-    image:VectorImg,
-    title:"Construction",
-    discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
-    },
-    {
-        image:fi_4671092,
-        title:"Automotive",
-        discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
-        },
-        {
-            image:basket,
-            title:"Retail",
-            discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
-            },
-            {
-                image:"truckter",
-                title:"Agriculture",
-                discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
-                },
-                {
-                    image:"Vector (9)",
-                    title:"Manufacturing",
-                    discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
-                    },
-                    {
-                        image:"Vector (9)",
-                        title:"Food & Beverage",
-                        discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
-                        }, {
-                            image:"Vector (9)",
-                            title:"Medical Equipment - Healthcare Products",
-                            discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
-                            },
+import VectorImg from "../../assets/images/Vector (9).png";
+import fi_4671092 from "../../assets/images/fi_4671092 (1).png";
+import basket from "../../assets/images/basket.png";
+import truckter from "../../assets/images/truckter.png";
+import Stroke from "../../assets/images/Stroke.png";
+import manufacturing from "../../assets/images/Manufacturing.png";
+import Beverage from "../../assets/images/Beverage.png";
+import Healthcare from "../../assets/images/Healthcare.png";
+import Aerospace from "../../assets/images/Aerospace.png";
+import Perishables from "../../assets/images/Perishables.png";
+import Chemicals from "../../assets/images/Chemicals.png";
+import Gaming from "../../assets/images/Gaming.png";
+import Trade from "../../assets/images/Trade.png";
+import Entertainment from "../../assets/images/Entertainment.png";
+import Electronics from "../../assets/images/Electronics.png";
+import Sports from "../../assets/images/Sports.png";
 
-                            {
-                                image:"Vector (9)",
-                                title:"Aerospace",
-                                discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
-                                },
+export const Box = [
+  {
+    image: VectorImg,
+    title: "Construction",
+    discription:
+      "Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.",
+  },
+  {
+    image: fi_4671092,
+    title: "Automotive",
+    discription:
+      "Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.",
+  },
+  {
+    image: basket,
+    title: "Retail",
+    discription:
+      "Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.",
+  },
+  {
+    image: truckter,
+    title: "Agriculture",
+    discription:
+      "Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.",
+  },
+  {
+    image: manufacturing,
+    title: "Manufacturing",
+    discription:
+      "Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.",
+  },
+  {
+    image: Beverage,
+    title: "Food & Beverage",
+    discription:
+      "Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.",
+  },
+  {
+    image: Healthcare,
+    title: "Medical Equipment - Healthcare Products",
+    discription:
+      "Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.",
+  },
 
-                                {
-                                    image:"Vector (9)",
-                                    title:"Perishables",
-                                    discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
-                                    },
+  {
+    image: Aerospace,
+    title: "Aerospace",
+    discription:
+      "Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.",
+  },
 
-                                    {
-                                        image:"Vector (9)",
-                                        title:"Chemicals & Plastics",
-                                        discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
-                                        },
+  {
+    image: Perishables,
+    title: "Perishables",
+    discription:
+      "Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.",
+  },
 
-                                        {
-                                            image:"Vector (9)",
-                                            title:"Gaming Equipment",
-                                            discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
-                                            },
+  {
+    image: Chemicals,
+    title: "Chemicals & Plastics",
+    discription:
+      "Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.",
+  },
 
-                                            {
-                                                image:"Vector (9)",
-                                                title:"Trade Shows",
-                                                discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
-                                                },
+  {
+    image: Gaming,
+    title: "Gaming Equipment",
+    discription:
+      "Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.",
+  },
 
-                                                {
-                                                    image:"Vector (9)",
-                                                    title:"Entertainment",
-                                                    discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
-                                                    },
+  {
+    image: Trade,
+    title: "Trade Shows",
+    discription:
+      "Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.",
+  },
 
-                                                    {
-                                                        image:"Vector (9)",
-                                                        title:"Technology & Electronics",
-                                                        discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
-                                                        },
+  {
+    image: Entertainment,
+    title: "Entertainment",
+    discription:
+      "Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.",
+  },
 
-                                                        {
-                                                            image:"Vector (9)",
-                                                            title:"Sports Equipment",
-                                                            discription:"Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
-                                                            },
+  {
+    image: Electronics,
+    title: "Sports Equipment",
+    discription:
+      "Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.",
+  },
 
+  {
+    image: Sports,
+    title: "Technology & Electronics",
+    discription:
+      "Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.",
+  },
+];
+export const linksList1 = [
+  {
+    image: Stroke,
+    titles: "Extensive Capacity",
+  },
+
+  {
+    image: Stroke,
+    titles: "Competitive Rates",
+  },
+
+  {
+    image: Stroke,
+    titles: "Dedicated Pricing Analyst",
+  },
+
+  {
+    image: Stroke,
+    titles: "End-to-end Visibility",
+  },
+];
+export const linksList2 = [
+  {
+    image: Stroke,
+    titles: "Modern, Reliable Equipment",
+  },
+
+  {
+    image: Stroke,
+    titles: "24/7 Coverage & Support",
+  },
+
+  {
+    image: Stroke,
+    titles: "Custom Solutions",
+  },
+
+  {
+    image: Stroke,
+    titles: "Client Focused Process",
+  },
 ];
