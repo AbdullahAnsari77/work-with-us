77239b81d9592974966ee1a5942b61baa28096ec
Abdullah <abdullah.iqqbal487@gmail.com>
2024-08-26 22:02:16 +0500

    responsive footer and header

--- src/component/links/index.jsx
+++ src/component/links/index.jsx
@@ -5,7 +5,7 @@ const LinkItem = ({ image, titles }) => {
   return (
     <div className="links">
       <a href="#">
-        <img src={image} alt="" />
+        <img className="widths" src={image} alt="" />
       </a>
       <h4 className="">{titles}</h4>
     </div>
